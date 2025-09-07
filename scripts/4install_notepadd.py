f68a2d7eaece86e36eafb0ae3c6c0dad3b236da0
Ankit kumar Vishwakarma <100838272+Ak-vishwakarma-01@users.noreply.github.com>
2025-09-07 19:11:49 +0530

    dede

--- scripts/4install_notepadd.py
+++ scripts/4install_notepadd.py
@@ -2,7 +2,7 @@ import subprocess
 import urllib.request
 
 # Download Notepad++ installer
-url = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/latest/download/npp.8.6.9.Installer.x64.exe"
+url = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.8.5/npp.8.8.5.Installer.x64.exe"
 installer_path = r"C:\Users\ak_vishwakarma_0\Downloads\npp_installer.exe"
 urllib.request.urlretrieve(url, installer_path)
 
